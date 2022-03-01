256e1838ac48b808129dfaedf49b033b66189153
Mjemmali <mjemmali@neiu.edu>
2022-02-28 19:49:51 -0800

    Hwk is done

--- GenericsAndCollections/Collections1/src/Task.java
+++ GenericsAndCollections/Collections1/src/Task.java
@@ -1,7 +1,6 @@
 import java.util.ArrayList;
-
 public class Task {
-    public static void main(String[] args){
+    public static void main(String[] args) {
         Deck d = new Deck();
         // Draw some random cards:
         System.out.println(d.dealCard());
@@ -12,18 +11,30 @@ public class Task {
         hand.add(d.dealCard());
         hand.add(d.dealCard());
 
-        System.out.println("Hand is:"+hand);
-        System.out.println("Cards add:"+addCards(hand));
+        System.out.println("Hand is:" + hand);
+        System.out.println("Cards add:" + addCards(hand));
 
-
-        for(Card /* TODO
-Compete a for loop that prints every card
-in the deck d */
-
+        for (Card tmp : d) {
+            System.out.print(tmp.toString());
+        }
     }
 
-    public static int addCards(ArrayList<Card> hand){
+    public static int addCards(ArrayList<Card> hand) {
  /* TODO 
 This should compute the sum of values of the cards in an ArrayList of cards.*/
+        int result = 0;
+        for (Card tmp : hand) {
+                if (tmp.getValue().toString().equals("Ace"))
+                    result += 1;
+                else if (tmp.getValue().toString().equals("Jack"))
+                    result += 11;
+                else if (tmp.getValue().toString().equals("Queen"))
+                    result += 12;
+                else if (tmp.getValue().toString().equals("King"))
+                    result += 13;
+                else
+                result += Integer.valueOf(tmp.getValue().toString());
+        }
+            return result;
     }
 }
--- GenericsAndCollections/Collections2/src/Counter.java
+++ GenericsAndCollections/Collections2/src/Counter.java
@@ -1,28 +1,44 @@
 import java.util.HashMap;
+import java.util.Map;
 import java.util.Set;
 
 public class Counter<E> {
-    private HashMap/* TODO 
-The HashMap should accomodate a generic class for the keys and Integer for the value*/ counts;
+    private HashMap<E, Integer> counts;
 
-    public Counter(E[] things){
+    public Counter(E[] things) {
 /* TODO
 Loop through the elements of "things" and use the HashMap "counts"
 to store the elements of things as keys, and the number of times they
-appear in the array as values. Check the instructions. */ }
+appear in the array as values. Check the instructions. */
+        counts = new HashMap<>();
+        for (E temp : things) {
+            if(counts.containsKey(temp))
+                counts.put(temp, counts.get(temp) + 1);
+            else
+                counts.put(temp,1);
+        }
     }
 
     public Integer getCount(E element){
       /* TODO 
 This just returns the specific count for an element. It is a wrapper
-for a getting the value of the HashMap given the key "element" */);
+for a getting the value of the HashMap given the key "element" */
+        return (Integer) counts.get(element);
+
     }
 
     public int getSize(){
         return this.counts.size();
     }
 
-    public E mostFrequent()/* TODO 
-Returns the most frequent key in the HashMap*/t;
+    public E mostFrequent() {
+        /* TODO
+Returns the most frequent key in the HashMap*/
+        Map.Entry<E, Integer> maxEntry = null;
+        for(Map.Entry<E, Integer> entry : counts.entrySet()){
+            if(maxEntry == null || entry.getValue() > maxEntry.getValue())
+                maxEntry = entry;
+        }
+        return maxEntry.getKey();
     }
 }
