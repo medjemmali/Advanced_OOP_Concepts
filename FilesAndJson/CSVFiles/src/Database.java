6152a54c29e17122bee465ec5948f50115a5c99b
Mohamed Jemmali <mjemmali@neiu.edu>
2022-03-10 02:15:16 -0600

    Finished part1

--- FilesAndJson/CSVFiles/src/Database.java
+++ FilesAndJson/CSVFiles/src/Database.java
@@ -28,14 +28,25 @@ public class Database {
     }
 
     public Database(String contents) {
-/* TODO 
+/* TODO
 This constructor should take the contents of a CSV file and initialize the memember variables of the Database class.
 */
+        String[] cont = contents.split("\\n");
+        colNames = cont[0].split(",");
+        numRows = cont.length - 1;
+        data = new String[numRows][colNames.length];
+        for (int i = 1; i < cont.length; i++)
+             data[i-1] = cont[i].split(",");
     }
-
     public String getValue(String columnName,int row){
-/* TODO */
-This method should return the data contained on row "row" and the column matching  @columname
+        int columnInd = 0;
+        for(int i = 0;i <colNames.length ; i++){
+                if(columnName.equals(colNames[i])){
+                    columnInd = i;
+                    break;
+                }
+            }
+        return data[row][columnInd];
     }
 
 }
