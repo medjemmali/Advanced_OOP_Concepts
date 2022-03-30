00828615f29f271e6d63b95f7cf734269fa005d8
Mohamed Jemmali <mjemmali@neiu.edu>
2022-03-29 22:31:20 -0500

    HWK Done

--- ThreadsAndConcurrency/RacingCars2/src/Racer.java
+++ ThreadsAndConcurrency/RacingCars2/src/Racer.java
@@ -1,3 +1,40 @@
-public class Racer /* TODO 
-This class is exactly as the Racer from Task 1*/
+public class Racer implements Runnable /* TODO
+Add something here so that you can pass this Racer to the constructor of Thread*/{
+    Car c;
+    int distance = 1000; // this is a racer that is going to run 1000 miles.
+
+    /* TODO
+Create a constructor that receives a Car objec and initializes c.*/
+
+    public Racer(Car c) {
+        this.c = c;
+    }
+
+    @Override
+    /* TODO
+Override one method.  This is the task the thread will perform... You have to determine which method this is.
+Check the method description. */
+    public void run() {
+
+        try {
+            while (c.odometer < distance)
+            {
+                System.out.println("In the while loop: " + c.toString());
+
+                if(c.miles == 0) {
+                    System.out.println("Charging");
+
+                    Thread.sleep((long)c.maxMiles);
+                    c.charge();
+
+                }
+                c.move(1);
+            }
+            System.out.println(c.name + " finished");
+        } catch (InterruptedException ie) {
+            ie.printStackTrace();
+        }
+
+
+    }
 }
